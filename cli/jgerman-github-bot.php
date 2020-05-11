ee00c6c684272bf8426160fc13cd0976888cf47d
zero-24 <zero-24@users.noreply.github.com>
2020-05-11 07:23:09 +0200

    make sure the constants are set before composer

--- cli/jgerman-github-bot.php
+++ cli/jgerman-github-bot.php
@@ -15,6 +15,9 @@ if (PHP_SAPI != 'cli')
 // Set error reporting for development
 error_reporting(-1);
 
+// Load the contstants
+require dirname(__DIR__) . '/includes/constants.php';
+
 // Ensure we've initialized Composer
 if (!file_exists(ROOT_PATH . '/vendor/autoload.php'))
 {
@@ -23,8 +26,7 @@ if (!file_exists(ROOT_PATH . '/vendor/autoload.php'))
 
 require ROOT_PATH . '/vendor/autoload.php';
 
-// Load the contstants and the github base configuration
-require dirname(__DIR__) . '/includes/constants.php';
+// Load the github base configuration
 require dirname(__DIR__) . '/includes/github-base.php';
 
 $logHelper->writeLogMessage('Start JGerman GitHub Bot');
